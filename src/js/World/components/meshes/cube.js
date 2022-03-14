d749c1fb370fa237ea672efe0341efe7168f9be3
birdlaketree <birdlaketree@gmail.com>
2022-03-14 14:48:34 +0100

    every cube has a rnd rotation speed

--- src/js/World/components/meshes/cube.js
+++ src/js/World/components/meshes/cube.js
@@ -3,10 +3,11 @@ import { BoxGeometry, Mesh } from 'three';
 const cube = material => {
   const geometry = new BoxGeometry();
   const mesh = new Mesh( geometry, material );
+  const speed = Math.random() + 0.4;
 
   mesh.tick = (delta) => {
-    mesh.rotation.x += delta * 0.8;
-    mesh.rotation.y += delta * 0.8;
+    mesh.rotation.x += delta * speed;
+    mesh.rotation.y += delta * speed;
   };
 
   return mesh;
